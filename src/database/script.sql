8a0a0c496df79ad78c597e44211f2535fecf1a7c
Daner-Quispe <daner.quispe@sptech.school>
2026-03-19 01:17:59 -0300

    resolvido conflito script.sql

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,9 +1,11 @@
+<<<<<<< Updated upstream
 CREATE DATABASE stefanini_quiz;
 USE stefanini_quiz;
 
 CREATE TABLE usuario(
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL
+
 );
 
 CREATE TABLE registro_quiz(
